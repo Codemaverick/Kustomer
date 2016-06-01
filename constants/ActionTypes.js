1af78a9db3a0f16ccd67c6b60335e2388206f356
tolu <tolu@digitalsketchpad.com>
2016-06-01 19:58:19 -0400

    create action type constants

---
+++ constants/ActionTypes.js
@@ -0,0 +1,11 @@
+// Bank Action Types
+const TransactionTypes  = {
+    CREDIT: 'credit',
+    DEBIT: 'debit',
+    TRANSFER: 'transfer'
+};
+
+export const ADD_TRANSACTION = 'add_transaction';
+export const CANCEL_TRANSACTION = 'cancel_transaction';
+
+export { TransactionTypes };
