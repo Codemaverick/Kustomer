c27af47fb1e708ce250ecef4236a3511e47af197
tolu <tolu@digitalsketchpad.com>
2016-06-04 13:38:20 -0400

    scaffold DataListView class

---
+++ assets/js/components/DataListView.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class DataListView extends 'Component' {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        
+    }
+}
+
+DataListView.defaultProps = {
+    
+};
+
+export default DataListView;
