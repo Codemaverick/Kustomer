6144ce0e1e2005b87f8576c69b6960481265ec73
Codemaverick <tolu@digitalsketchpad.com>
2016-06-04 22:55:02 -0400

    update application build config

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,19 @@
  * pack the web.
  */
 module.exports = {
-    entry: './assets/js/app.js',
+    entry: './assets/js/Application.js',
     output: {
         filename: './assets/js/build/app.js'
     },
     module: {
         loaders: [{
-            test: /\.es6$|\.js$/,
+            test: /\.es6$|\.js$|\.jsx$/,
             exclude: /(node_modules)/,
             loader: 'babel-loader'
         }]
     },
     resolve: {
-        extensions: ['', '.js','.es6']
+        extensions: ['', '.js','.es6','.jsx']
     },
     query: {
         presets: ['es2015']
