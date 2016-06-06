0f5d265f8745be390fcc8b6ec07df7ce4804380d
Codemaverick <tolu@digitalsketchpad.com>
2016-06-06 01:57:30 -0400

    refactor account reducer to only generate transaction for debit and credit types

--- assets/js/reducers/Reducers.js
+++ assets/js/reducers/Reducers.js
@@ -2,40 +2,65 @@ import { TransactionTypes, RequestType } from '../constants/ActionTypes';
 import { combineReducers } from 'redux';
 
 const initState = {
-    balance: 500.00,
-    transactions: [],
+    account: {
+        balance: 500.00,
+        transactions: [
+            {type:TransactionTypes.CREDIT, amount: 500.00, success: true, date: new Date(), id: '2016-00'}
+        ]
+    },
     transactionRequest: {
         text: '',
         showTransactionInput: false,
-        currentRequest: null
+        currentRequest: null,
+        error: ''
     }
 }
 //need to account for error state if debit amount > balance
-function balance(state = initState.balance, action) {
-    let new_balance;
-
+function account(state = initState.account, action) {
+    let new_balance, success;
+    
     switch (action.type) {
         case TransactionTypes.CREDIT:
-            new_balance = state + action.amount;
-            break;
+            new_balance = state.balance + action.amount;
+            success = true;
+            
+            return Object.assign({}, state, { 
+                balance: new_balance,
+                transactions: [...state.transactions, {
+                    type:action.type, 
+                    amount: action.amount, 
+                    success: success, 
+                    date: action.date,
+                    id: action.id
+                }]
+            });
+           
 
         case TransactionTypes.DEBIT:
-            new_balance = action.amount > state ? state : state - action.amount;
-            break;
+            if (action.amount > state.balance) {
+                new_balance = state.balance;
+                success = false;
+            }
+            else {
+                new_balance = state.balance - action.amount;
+                success = true;
+            }
+            return Object.assign({}, state, { 
+                balance: new_balance,
+                transactions: [...state.transactions, {
+                    type:action.type, 
+                    amount: action.amount, 
+                    success: success, 
+                    date: action.date,
+                    id: action.id
+                }]
+            });
 
-        default:
-            new_balance = state;
-    }
-
-    return new_balance;
-}
-
-function transactions(state = [], action) {
-    switch (action.type) {
         default:
             return state;
     }
 }
+
 
 function transactionRequest(state = initState.transactionRequest, action) {
     switch (action.type) {
@@ -43,13 +68,16 @@ function transactionRequest(state = initState.transactionRequest, action) {
          return Object.assign({}, state, {showTransactionInput: true, text:'Enter Credit Amount:', currentRequest: RequestType.CREDIT} );
         case RequestType.DEBIT:
             return Object.assign({}, state, {showTransactionInput: true, text:'Enter Debit Amount:', currentRequest: RequestType.DEBIT} );
+        case RequestType.ERROR:
+            return Object.assign({}, state, {error: action.message});
         default:
             return initState.transactionRequest;
     }
 }
 
+
 const BankApp = combineReducers({
-    balance, transactions, transactionRequest
+    account, transactionRequest
 });
 
 export default BankApp;
