234e2c31723e458ffb252bab92864cf79c696fa3
Codemaverick <tolu@digitalsketchpad.com>
2016-06-06 02:27:33 -0400

    minor refactor of action types

--- assets/js/constants/ActionTypes.js
+++ assets/js/constants/ActionTypes.js
@@ -1,15 +1,14 @@
 // Bank Action Types
 const TransactionTypes  = {
-    CREDIT: 'credit',
-    DEBIT: 'debit',
-    TRANSFER: 'transfer'
+    CREDIT: 'CREDIT',
+    DEBIT: 'DEBIT'
 };
 
 const RequestType = {
-    CREDIT: 'request_credit',
-    DEBIT: 'request_debit',
-    CANCEL: 'request_cancel',
-    ERROR: 'request_error'
+    CREDIT: 'REQUEST_CREDIT',
+    DEBIT: 'REQUEST_DEBIT',
+    CANCEL: 'REQUEST_CANCEL',
+    ERROR: 'REQUEST_ERROR'
 };
 
 export const ADD_TRANSACTION = 'add_transaction';
