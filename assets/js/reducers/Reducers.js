0891076d61c90d04c6cd95c0df4b61ae6bf166ba
Codemaverick <tolu@digitalsketchpad.com>
2016-06-05 23:00:04 -0400

    Refactor transaction requests to be reduced by one function

--- assets/js/reducers/Reducers.js
+++ assets/js/reducers/Reducers.js
@@ -1,39 +1,55 @@
-import { TransactionTypes } from '../constants/ActionTypes';
+import { TransactionTypes, RequestType } from '../constants/ActionTypes';
 import { combineReducers } from 'redux';
 
 const initState = {
     balance: 500.00,
-    transactions: []
+    transactions: [],
+    transactionRequest: {
+        text: '',
+        showTransactionInput: false,
+        currentRequest: null
+    }
 }
 //need to account for error state if debit amount > balance
 function balance(state = initState.balance, action) {
     let new_balance;
-    
-    switch(action.type) {
+
+    switch (action.type) {
         case TransactionTypes.CREDIT:
             new_balance = state + action.amount;
             break;
-                
+
         case TransactionTypes.DEBIT:
-            new_balance =  action.amount > state ? state : state - action.amount;
+            new_balance = action.amount > state ? state : state - action.amount;
             break;
-            
-        default: 
+
+        default:
             new_balance = state;
     }
-    
+
     return new_balance;
 }
 
 function transactions(state = [], action) {
-    switch(action.type) {
+    switch (action.type) {
         default:
             return state;
     }
 }
 
+function transactionRequest(state = initState.transactionRequest, action) {
+    switch (action.type) {
+        case RequestType.CREDIT:
+         return Object.assign({}, state, {showTransactionInput: true, text:'Enter Credit Amount:', currentRequest: RequestType.CREDIT} );
+        case RequestType.DEBIT:
+            return Object.assign({}, state, {showTransactionInput: true, text:'Enter Debit Amount:', currentRequest: RequestType.DEBIT} );
+        default:
+            return initState.transactionRequest;
+    }
+}
+
 const BankApp = combineReducers({
-   balance, transactions
+    balance, transactions, transactionRequest
 });
 
 export default BankApp;
